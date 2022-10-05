2edba5d9f69d11b73a2e48b730d8b08dd9cbfb62
SpiritualForest <meditationlover@gmail.com>
2022-10-05 21:27:45 +0300

    Added pressing and releasing animation to CircleButton view.

--- app/src/main/java/com/androidtetris/activity/tetris/CircleButton.kt
+++ app/src/main/java/com/androidtetris/activity/tetris/CircleButton.kt
@@ -5,6 +5,7 @@ import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Point
+import android.graphics.PointF
 import android.util.AttributeSet
 import android.view.View
 import android.view.MotionEvent
@@ -14,45 +15,101 @@ import kotlin.math.min
 import com.google.android.material.color.MaterialColors
 import com.androidtetris.R
 
+import android.util.Log
+
+/* NOTE:
+ * the actual functionality events for this button
+ * are handled in TetrisActivity, not here.
+ * This view only handles its own drawing.
+ * See TetrisActivity to see the various gameplay functions
+ * that are hooked to CircleButton views. */
+
 class CircleButton(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
     private val paint = Paint()
-    private var currentColor = MaterialColors.getColor(this, R.attr.colorOnPrimary)
+    private val pressedColor = MaterialColors.getColor(this, R.attr.colorPrimaryVariant) // Colour when pressed
+    private var unpressedColor = MaterialColors.getColor(this, R.attr.colorOnPrimary) // Colour when not pressed
     private val mHandler = Handler(Looper.getMainLooper())
-    private var radii: MutableList<Float> = mutableListOf() // List of radii to draw
+    private var drawOutwards = false // If true, draw circles from the center outwards. Otherwise, rollback.
+    private var currentCircle = 0f // Which circle we're currently at. 0 to 1 with steps of 0.1
+    private var drawingRunnable = DrawingRunnable(this, mHandler)
 
     override fun onDraw(canvas: Canvas) {
-        // Draw a circle that spans as close as possible to the canvas's size
-        paint.color = currentColor
         val center = Point(width / 2, height / 2)
-        val completeRadius = min(center.x, center.y) // The largest radius, a "complete" canvas
-        val smallestRadius = 0.2f*completeRadius // 20% of the largest
+        val completeRadius = min(center.x, center.y)
+        
+        if (drawOutwards) {
+            // Draw outwards until we reach completeRadius
+            if (currentCircle < 1f) {
+                currentCircle += 0.1f;
+            }
+            else {
+                // No more circles to draw after this
+                drawingRunnable.stop()
+            }
+        }
+        else {
+            // Draw inwards, circles shrinking, towards the center.
+            if (currentCircle > 0f) {
+                currentCircle -= 0.1f;
+            }
+            else {
+                // No more circles to draw after this
+                drawingRunnable.stop()
+            }
+        }
+        // Draw the whole background circle, first (unpressed colour)
+        paint.color = unpressedColor
         canvas.drawCircle(center.x.toFloat(), center.y.toFloat(), completeRadius.toFloat(), paint)
+        // Now draw the circle
+        val radius = currentCircle * completeRadius.toFloat()
+        paint.color = pressedColor
+        canvas.drawCircle(center.x.toFloat(), center.y.toFloat(), radius, paint)
     }
 
     override fun onTouchEvent(ev: MotionEvent): Boolean {
-        val x: Float = ev.x
-        val y: Float = ev.y
         when (ev.action) {
             MotionEvent.ACTION_DOWN -> {
-                currentColor = MaterialColors.getColor(this, R.attr.colorPrimaryVariant)
-                drawButtonDown()
+                currentCircle = 0f
+                drawOutwards = true
+                drawingRunnable.restart()
             }
             MotionEvent.ACTION_UP -> {
-                currentColor = MaterialColors.getColor(this, R.attr.colorOnPrimary)
-                drawButtonUp()
+                drawOutwards = false
+                drawingRunnable.restart()
             }
         }
-        invalidate()
         return true
     }
+}
 
-    fun drawButtonDown() {
-        // Draw the animation when the button is touched
-        radii = mutableListOf()
+class DrawingRunnable(private val mButton: CircleButton, private val mHandler: Handler) : Runnable {
+    /* All this runnable does is call invalidate() repeatedly on
+     * the given CircleButton view */
+
+    private var runCode = false // Don't run by default
+    
+    override fun run() {
+        // Run the code
+        if (runCode) {
+            mHandler.postDelayed(this, 2L)
+            mButton.invalidate()
+        }
     }
 
-    fun drawButtonUp() {
-        // Draw the animation when the button is released
-        radii = mutableListOf()
+    fun stop() {
+        // Stop executing this runnable
+        runCode = false
+        mHandler.removeCallbacks(this)
+    }
+
+    fun start() {
+        // Start executing this runnable
+        runCode = true
+        run()
+    }
+
+    fun restart() {
+        stop()
+        start()
     }
 }
